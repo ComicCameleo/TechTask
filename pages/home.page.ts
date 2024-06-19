c408b2f61da693cfdb7b1e3cebd68123dd69a521
ComicCameleo <comcam13@gmail.com>
2024-06-19 15:34:28 +0300

    Update home.page.ts
    Remove login element from home page

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,23 +1,16 @@
 import { expect, Locator, Page } from '@playwright/test';
 
 export default class HomePage {
-    private readonly loginButton: Locator
     private readonly newOrderButton: Locator
     private readonly closeAllOrdersButton: Locator
     readonly positionCounterLabel: Locator
     readonly closeOrderButtonList: Locator
 
     constructor(page: Page) {
-        this.loginButton = page.getByTestId("log-in");
         this.newOrderButton = page.locator("//*[@id ='ic_new_order']/ancestor::button");
         this.positionCounterLabel = page.locator("//*[text()='Positions']/div");
         this.closeOrderButtonList = page.locator("//*[@id='ic_access_cross']");
         this.closeAllOrdersButton = page.locator("//button//*[contains(text(),'Close')]");
-    }
-
-    async openLoginPopup()
-    {
-        await this.loginButton.click();
     }
 
     async openNewOrderPopUp()
@@ -27,9 +20,6 @@ export default class HomePage {
 
     async cleanAllOrders()
     {
-        if(await this.closeAllOrdersButton.isEnabled())
-        {
-            await this.closeAllOrdersButton.click();
-        }
+        await this.closeAllOrdersButton.click();
     }
 }
